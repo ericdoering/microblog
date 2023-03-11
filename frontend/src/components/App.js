c1346b2ae77de4035d3e00cf80d5bcd0534f4db0
Eric Doering <ericbenjamindoering@gmail.com>
2023-03-11 14:24:32 -0500

    setup of actions

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,35 @@
 import React from "react";
+import NewPost from "./NewPost";
+import { Route, NavLink, Switch } from "react-router-dom"
+import Post from "./Post";
+import Home from "./Home"
 
 function App() {
+
   return (
-<p>hi</p>
-  );
-}
+    <div className="App-container">
+      <header className="App-header jumbotron mt-2">
+        <h1 className="App-title display-4">Microblog</h1>
+        <p className="lead">Spring into blogging</p>
+        <nav>
+          <NavLink exact to="/">Blog</NavLink>
+          <NavLink exact to="/new">Add a new post</NavLink>
+        </nav>
+      </header>
+
+      <Switch>
+        <Route exact path="/new">
+          <NewPost />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/:postId">
+          <Post />
+        </Route>
+      </Switch>
+    </div>
+  )
+};
 
 export default App;
